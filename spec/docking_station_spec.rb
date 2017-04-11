3ff49e122d3baadb244c5d32c0d27e115c014c27
Tim Hawkins <timothyhawkins@Timothys-MacBook-Pro.local>
2017-04-11 16:40:38 +0100

    testing for a new instance of bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,9 @@ require "docking_station"
 
 describe DockingStation do
   it "responds to release bike" do
-    expect((DockingStation.new).release_bike).to eq true
+    expect((DockingStation.new).release_bike).to be_a Bike
+  end
+  it "checks whether the bike is working" do
+    expect((Bike.new).working?).to eq true
   end
 end
