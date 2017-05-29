13ae1dd802866da3b0105254f8903457206ef51e
Filipe Fasolin <ffasolin@gmail.com>
2017-05-29 22:30:11 +0100

    Vans send fixed bikes to stations

--- lib/vans.rb
+++ lib/vans.rb
@@ -3,4 +3,9 @@ class Vans # :nodoc:
   def ships_to_garage(broken_bikes)
     @bikes_in_van = broken_bikes
   end
+
+  def ships_to_station(fixed_bikes, station_bikes)
+    @bikes_in_van = fixed_bikes
+    fixed_bikes.each { |bike| station_bikes << bike }
+  end
 end
--- spec/vans_spec.rb
+++ spec/vans_spec.rb
@@ -1,4 +1,7 @@
 require 'vans'
+require 'docking_station'
+require 'bike'
+require 'garage'
 
 describe Vans do
   it 'takes broken bikes on the van' do
@@ -7,5 +10,16 @@ describe Vans do
     docking_station.dock(Bike.new)
     subject.ships_to_garage(docking_station.broken_bikes)
     expect(subject.bikes_in_van.count).to eq 1
+  end
+
+  fit 'takes fixed bikes back to docking station' do
+    docking_station = DockingStation.new
+    garage = Garage.new
+    10.times { docking_station.release_bike }
+    docking_station.dock(Bike.new)
+    subject.ships_to_garage(docking_station.broken_bikes)
+    garage.fix(subject.bikes_in_van)
+    subject.ships_to_station(garage.fixed_bikes, docking_station.bikes)
+    expect(docking_station.bikes.count).to eq 11
   end
 end
